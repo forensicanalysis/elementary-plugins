dfcd5c08fed044967d278495e0cb613a9725ba29
Jonas Plum <git@cugu.eu>
2020-06-07 01:14:10 +0200

    Change license

--- yara/yara_plugin.py
+++ yara/yara_plugin.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Siemens AG
+# Copyright (c) 2020 Jonas Plum
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
